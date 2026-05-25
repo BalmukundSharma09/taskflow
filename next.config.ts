e2675ecdbf98cb81aa2ae34924a71cbfac7f71ea
BSharma <balmukund@kenmarkitan.com>
2026-05-25 12:47:18 +0530

    Third commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["bcryptjs"],
+  env: {
+    AUTH_URL: process.env.RAILWAY_PUBLIC_DOMAIN
+      ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`
+      : process.env.AUTH_URL || "http://localhost:3000",
+    NEXTAUTH_URL: process.env.RAILWAY_PUBLIC_DOMAIN
+      ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`
+      : process.env.NEXTAUTH_URL || "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
